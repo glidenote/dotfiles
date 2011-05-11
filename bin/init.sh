1905d0dea690b83a71513ec2a36d80646d8c4f59
glidenote <glidenote@gmail.com>
2011-05-11 12:52:44 +0900

    ファイルリストをまとめた

--- bin/init.sh
+++ bin/init.sh
@@ -3,6 +3,22 @@
 # git clone git://github.com/glidenote/dotfiles.git 
 # の後に流し、ホームディレクトリをgithubに置いてあるものに置き換える
 
+FILELIST="
+.vim
+bin
+.bash_profile
+.git-completion.bash
+bash_completion_tmux.sh
+.gitignore
+.screenrc
+.tscreenrc
+.vimrc
+.zshrc
+.bashrc
+bash_completion_tmux.sh
+.tmux.conf
+"
+
 cd ~
 
 # 必要ディレクトリの作成
@@ -13,53 +29,21 @@ touch ~/.zsh/`hostname`.rc
 # 既存ディレクトリ、ファイルを待避させる
 if [ ! -e ~/old_files ]; then
 	mkdir ~/old_files
-	mv ~/bin ~/old_files 
-	mv ~/.vim~ /old_files
-	mv ~/.bash_profile ~/old_files
-	mv ~/.bashrc ~/old_files 
-	mv ~/.git ~/old_files 
-	mv ~/git-completion.bash ~/old_files 
-	mv ~/.gitconfig ~/old_files 
-	mv ~/.gitignore ~/old_files
-	mv ~/.gitmodules ~/old_files 
-	mv ~/.screenrc ~/old_files 
-	mv ~/.tscreenrc ~/old_files 
-	mv ~/.vimrc ~/old_files  
-	mv ~/.zshrc ~/old_files  
-	mv ~/.tmux.conf ~/old_files  
+	for FILE in ${FILELIST};
+	do
+		mv ~/${FILE} ~/old_files/
+	done
 fi
+
+# link張り直し
+for FILE in ${FILELIST};
+do
+	/bin/rm -rf ~/${FILE}
+	ln -s ~/dotfiles/${FILE} ~/${FILE}
+done
 
 # submodule
 cd ~/dotfiles
 git submodule init
 git submodule update
 
-# linkを貼りなおし
-/bin/rm -rf ~/.vim
-/bin/rm -rf ~/bin
-/bin/rm -rf ~/.bash_profile
-/bin/rm -rf ~/.git-completion.bash
-/bin/rm -rf ~/bash_completion_tmux.sh
-/bin/rm -rf ~/.gitignore
-/bin/rm -rf ~/.screenrc
-/bin/rm -rf ~/.tscreenrc
-/bin/rm -rf ~/.vimrc
-/bin/rm -rf ~/.zshrc
-/bin/rm -rf ~/.bashrc
-/bin/rm -rf ~/bash_completion_tmux.sh
-/bin/rm -rf ~/.tmux.conf
-
-ln -s ~/dotfiles/.vim ~/
-ln -s ~/dotfiles/bin ~/
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.git-completion.bash ~/.git-completion.bash
-ln -s ~/dotfiles/.gitignore ~/.gitignore
-ln -s ~/dotfiles/.screenrc ~/.screenrc
-ln -s ~/dotfiles/.tscreenrc ~/.tscreenrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/bash_completion_tmux.sh ~/bash_completion_tmux.sh
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-
-# ln -s ~/dotfiles/.gitconfig ~/.gitconfig
