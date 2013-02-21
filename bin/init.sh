621709bf686e0e761dbde2d003480e27e4b9cb17
Akira Maeda <glidenote@gmail.com>
2013-02-21 18:17:38 +0900

    vim-proc用のpost-mergeが利用出来なくなっていたので修正

--- bin/init.sh
+++ bin/init.sh
@@ -59,6 +59,6 @@ vim +BundleInstall! +qall
 
 # vimproc make task
 cd ~/.vim/bundle/vimproc/.git/hooks/
-wget https://raw.github.com/gist/2823168/55efc9c72bd9bda4ea6a6af3a37daf253957753f/post-merge
+wget https://gist.github.com/glidenote/2823168/raw/55efc9c72bd9bda4ea6a6af3a37daf253957753f/post-merge
 chmod 755 post-merge
 ./post-merge
