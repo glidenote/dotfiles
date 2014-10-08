0f3d86489c74c44ee87979344c99a7fdd362a801
Akira Maeda <glidenote@gmail.com>
2014-10-08 17:21:02 +0900

    ec2lsに変更

--- .zsh.d/peco-sources/peco-ec2ssh.zsh
+++ .zsh.d/peco-sources/peco-ec2ssh.zsh
@@ -1,5 +1,5 @@
 function peco-ec2ssh () {
-    local selected_dir=$(ec2list | peco --query "$LBUFFER" | cut -f 4)
+    local selected_dir=$(ec2ls | peco --query "$LBUFFER" | cut -f 4)
     if [ -n "$selected_dir" ]; then
         BUFFER="ssh ${selected_dir}"
         zle accept-line
