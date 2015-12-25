3e9bf67f21508946d069590f71eff1f88934dbe3
Akira Maeda <glidenote@users.noreply.github.com>
2015-12-25 17:29:52 +0900

    update

--- .zsh.d/mac.zsh
+++ .zsh.d/mac.zsh
@@ -4,19 +4,11 @@ alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"
 
 if [[ -f `command -v direnv` ]] ; then eval "$(direnv hook zsh)" ; fi
 
-# for nodebrew
-export PATH=$HOME/.nodebrew/current/bin:$PATH
+# for nvm
+export NVM_DIR=~/.nvm
+. $(brew --prefix nvm)/nvm.sh
 
 if [[ -f ~/bin/aws_zsh_completer.sh ]] ; then source ~/bin/aws_zsh_completer.sh; fi
-
-if which plenv > /dev/null; then eval "$(plenv init -)"; fi
-
-# perl
-export PERL_LOCAL_LIB_ROOT="/Users/`whoami`/perl5:$PERL_LOCAL_LIB_ROOT";
-export PERL_MB_OPT="--install_base "/Users/`whoami`/perl5"";
-export PERL_MM_OPT="INSTALL_BASE=/Users/`whoami`/perl5";
-export PERL5LIB="/Users/`whoami`/perl5/lib/perl5:$PERL5LIB";
-export PATH="/Users/`whoami`/perl5/bin:$PATH";
 
 # http://rcmdnk.github.io/blog/2014/11/22/computer-mac-homebrew/
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
